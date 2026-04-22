ac78660e41a7a5f43f1f3cda2e0384e8ba1c4ac9
Junior Busso <juniorbusso@gmail.com>
2026-04-22 20:54:11 -0300

    Segundo cambio realizado, agregamos un if por si agrega un monto negativo

--- Refactorizar/Inicial.py
+++ Refactorizar/Inicial.py
@@ -1,13 +1,15 @@
 opcion = 0
 saldo = 10000
+DATOS_CONSUMIDOS = 30
 
-while opcion != 4:
+while opcion != 5:
 
     print("\n--- MENÚ PRINCIPAL ---")
     print("1. Consultar saldo")
     print("2. Ver promociones")
     print("3. Recargar saldo")
-    print("4. Salir")
+    print("4. Consultar datos consumidos")
+    print("5. Salir")
 
     opcion = int(input("Seleccione una opción: "))
 
@@ -21,11 +23,17 @@ while opcion != 4:
 
     elif opcion == 3:
         monto = int(input("Ingrese monto a recargar: "))
-        saldo = saldo + monto
-        print("Recarga exitosa. Nuevo saldo: $", saldo)
-
+        if monto < 0:
+            print("Ingrese un monto positivo")
+        else:
+            saldo = saldo + monto
+            print("Recarga exitosa. Nuevo saldo: $", saldo)
+    
     elif opcion == 4:
+        print("\nDatos consumidos: ", DATOS_CONSUMIDOS)
+    
+    elif opcion == 5:
         print("\nGracias por comunicarse. ¡Hasta luego!")
-
+    
     else:
         print("Opción inválida. Intente nuevamente.")
