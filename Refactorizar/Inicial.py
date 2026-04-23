f46554b663e1bc02fb383337c18086c54a72b65e
TobiasZoia <tobias3era@hotmail.com>
2026-04-23 13:29:55 -0300

    Agregado contador de operaciones realizadas por el usuario

--- Refactorizar/Inicial.py
+++ Refactorizar/Inicial.py
@@ -2,7 +2,7 @@ opcion = 0
 saldo = 10000
 consumido = 500
 i=0
-
+operaciones=0
 while opcion != 5:
 
     print("\n--- MENÚ PRINCIPAL ---")
@@ -16,15 +16,18 @@ while opcion != 5:
 
     if opcion == 1:
         print("\nSu saldo actual es: $", saldo)
+        operaciones+=1
 
     elif opcion == 2:
         print("\nPromociones disponibles:")
         print("- 2x1 en recargas")
         print("- 30% de descuento en datos móviles")
+        operaciones+=1
 
     elif opcion == 3:
         if i >= 3:
             print("No podés hacer más recargas")
+            operaciones+=1
         else:
             recarga=1
             while i < 3 and recarga == 1:
@@ -33,6 +36,7 @@ while opcion != 5:
                     monto = int(input("Ingrese un monto válido: "))
                 saldo+=monto
                 i+=1
+                operaciones+=1
                 print("Recarga exitosa. Nuevo saldo: $", saldo)
                 if i==1:
                     recarga = int(input("Te quedan 2 recargas. Presiona 1 para cargar de nuevo, 2 para volver al menú: "))
@@ -42,10 +46,12 @@ while opcion != 5:
                     print("No quedan más recargas")      
 
     elif opcion == 4:
-        print("Consumiste:", consumido)    
+        print("Consumiste:", consumido)
+        operaciones+=1    
 
     elif opcion == 5:
         print("\nGracias por comunicarse. ¡Hasta luego!")
+        print("\nHiciste", operaciones, "operaciones")
 
     else:
         print("Opción inválida. Intente nuevamente.")
