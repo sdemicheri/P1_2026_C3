2208011173a8768e31584c8376e21efd485195db
Miqueas17 <miqueasbarrozo12@gmail.com>
2026-04-22 21:20:12 -0300

    Try_except

--- Refactorizar/Inicial.py
+++ Refactorizar/Inicial.py
@@ -6,42 +6,45 @@ recargas=3
 
 while opcion != 5:
 
-    print("\n--- MENÚ PRINCIPAL ---")
-    print("1. Consultar saldo")
-    print("2. Ver promociones")
-    print("3. Recargar saldo")
-    print("4. Consuta datos consumidos")
-    print("5. Salir")
+    try:
+        print("\n--- MENÚ PRINCIPAL ---")
+        print("1. Consultar saldo")
+        print("2. Ver promociones")
+        print("3. Recargar saldo")
+        print("4. Consuta datos consumidos")
+        print("5. Salir")
 
-    opcion = int(input("Seleccione una opción: "))
-    contador+=1
+        opcion = int(input("Seleccione una opción: "))
+        contador+=1
 
-    if opcion == 1:
-        print("\nSu saldo actual es: $", saldo)
+        if opcion == 1:
+            print("\nSu saldo actual es: $", saldo)
 
-    elif opcion == 2:
-        print("\nPromociones disponibles:")
-        print("- 2x1 en recargas")
-        print("- 30% de descuento en datos móviles")
+        elif opcion == 2:
+            print("\nPromociones disponibles:")
+            print("- 2x1 en recargas")
+            print("- 30% de descuento en datos móviles")
 
-    elif opcion == 3:
-        if recargas != 0:
-            monto = int(input("Ingrese monto a recargar: "))
-            while monto < 0:
-                print("Ingrese un valor valido")
-                monto = int(input("Ingrese monto a recargar: "))            
-            saldo = saldo + monto
-            print("Recarga exitosa. Nuevo saldo: $", saldo)
-            recargas-=1
+        elif opcion == 3:
+            if recargas != 0:
+                monto = int(input("Ingrese monto a recargar: "))
+                while monto < 0:
+                    print("Ingrese un valor valido")
+                    monto = int(input("Ingrese monto a recargar: "))            
+                saldo = saldo + monto
+                print("Recarga exitosa. Nuevo saldo: $", saldo)
+                recargas-=1
+            else:
+                print("Supero la cantidad de recargas disponibles")
+
+        elif opcion == 4:
+            print("\nSus datos consumidos son: $", DATOS)
+
+        elif opcion == 5:
+            print("\nGracias por comunicarse. ¡Hasta luego!")
+            print("Realizaste", contador-1, "operaciones")
+
         else:
-            print("Supero la cantidad de recargas disponibles")
-
-    elif opcion == 4:
-        print("\nSus datos consumidos son: $", DATOS)
-
-    elif opcion == 5:
-        print("\nGracias por comunicarse. ¡Hasta luego!")
-        print("Realizaste", contador-1, "operaciones")
-
-    else:
+            print("Opción inválida. Intente nuevamente.")
+    except ValueError:
         print("Opción inválida. Intente nuevamente.")
