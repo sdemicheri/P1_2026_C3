801b9007b06512dc64ea09713e88cb56f397d53b
Junior Busso <juniorbusso@gmail.com>
2026-04-22 21:02:16 -0300

    Tercer cambio realizado, agregamos una variable de interacciones y al salir del programa te dice cuantas tuviste

--- Refactorizar/Inicial.py
+++ Refactorizar/Inicial.py
@@ -1,6 +1,7 @@
 opcion = 0
 saldo = 10000
 DATOS_CONSUMIDOS = 30
+interacciones = 0
 
 while opcion != 5:
 
@@ -15,12 +16,14 @@ while opcion != 5:
 
     if opcion == 1:
         print("\nSu saldo actual es: $", saldo)
+        interacciones = interacciones + 1
 
     elif opcion == 2:
         print("\nPromociones disponibles:")
         print("- 2x1 en recargas")
         print("- 30% de descuento en datos móviles")
-
+        interacciones = interacciones + 1
+        
     elif opcion == 3:
         monto = int(input("Ingrese monto a recargar: "))
         if monto < 0:
@@ -28,12 +31,15 @@ while opcion != 5:
         else:
             saldo = saldo + monto
             print("Recarga exitosa. Nuevo saldo: $", saldo)
-    
+        interacciones = interacciones + 1    
+        
     elif opcion == 4:
         print("\nDatos consumidos: ", DATOS_CONSUMIDOS)
-    
+        interacciones = interacciones + 1
+        
     elif opcion == 5:
         print("\nGracias por comunicarse. ¡Hasta luego!")
+        print("Tu cantidad de interacciones fue:", interacciones) 
     
     else:
         print("Opción inválida. Intente nuevamente.")
