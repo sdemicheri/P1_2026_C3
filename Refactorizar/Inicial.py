03ecdef7fc8fb4568afce2773ba468d9728b1ed8
Junior Busso <juniorbusso@gmail.com>
2026-04-22 21:15:52 -0300

    Sexto cambio, agregamos un try except para hacer un codigo mas robusto

--- Refactorizar/Inicial.py
+++ Refactorizar/Inicial.py
@@ -3,51 +3,53 @@ saldo = 10000
 DATOS_CONSUMIDOS = 30
 interacciones = 0
 recargas = 0
+try:
+    while opcion != 5:
 
-while opcion != 5:
+        print("\n--- MENÚ PRINCIPAL ---")
+        print("1. Consultar saldo")
+        print("2. Ver promociones")
+        print("3. Recargar saldo")
+        print("4. Consultar datos consumidos")
+        print("5. Salir")
 
-    print("\n--- MENÚ PRINCIPAL ---")
-    print("1. Consultar saldo")
-    print("2. Ver promociones")
-    print("3. Recargar saldo")
-    print("4. Consultar datos consumidos")
-    print("5. Salir")
+        opcion = int(input("Seleccione una opción: "))
 
-    opcion = int(input("Seleccione una opción: "))
-
-    match opcion:
+        match opcion:
         
-        case 1:
-            print("\nSu saldo actual es: $", saldo)
-            interacciones = interacciones + 1
-
-        case 2:
-            print("\nPromociones disponibles:")
-            print("- 2x1 en recargas")
-            print("- 30% de descuento en datos móviles")
-            interacciones = interacciones + 1
-        
-        case 3:
-            recargas = recargas + 1
-            if recargas < 3:
-                monto = int(input("Ingrese monto a recargar: "))
-                if monto < 0:
-                    print("Ingrese un monto positivo")
-                else:
-                    saldo = saldo + monto
-                print("Recarga exitosa. Nuevo saldo: $", saldo)
+            case 1:
+                print("\nSu saldo actual es: $", saldo)
                 interacciones = interacciones + 1
-            else:
-                print("Cantidad de recargas superadas")
+
+            case 2:
+                print("\nPromociones disponibles:")
+                print("- 2x1 en recargas")
+                print("- 30% de descuento en datos móviles")
+                interacciones = interacciones + 1
+        
+            case 3:
+                recargas = recargas + 1
+                if recargas < 3:
+                    monto = int(input("Ingrese monto a recargar: "))
+                    if monto < 0:
+                        print("Ingrese un monto positivo")
+                    else:
+                        saldo = saldo + monto
+                    print("Recarga exitosa. Nuevo saldo: $", saldo)
+                    interacciones = interacciones + 1
+                else:
+                    print("Cantidad de recargas superadas")
             
         
-        case 4:
-            print("\nDatos consumidos: ", DATOS_CONSUMIDOS)
-            interacciones = interacciones + 1
+            case 4:
+                print("\nDatos consumidos: ", DATOS_CONSUMIDOS)
+                interacciones = interacciones + 1
         
-        case 5:
-            print("\nGracias por comunicarse. ¡Hasta luego!")
-            print("Tu cantidad de interacciones fue:", interacciones) 
+            case 5:
+                print("\nGracias por comunicarse. ¡Hasta luego!")
+                print("Tu cantidad de interacciones fue:", interacciones) 
     
-        case _:
-            print("Opción inválida. Intente nuevamente.")
+            case _:
+                print("Opción inválida. Intente nuevamente.")
+except ValueError:
+    print("Valor no valido, ingrese un valor valido")
