87ce7964a8c71bc8814699b24d318980336bd305
Junior Busso <juniorbusso@gmail.com>
2026-04-22 21:13:16 -0300

    Quinto cambio, modificamos los condicionales multiples elif por el match, haciendo un codigo mas limpio y legible

--- Refactorizar/Inicial.py
+++ Refactorizar/Inicial.py
@@ -15,37 +15,39 @@ while opcion != 5:
 
     opcion = int(input("Seleccione una opción: "))
 
-    if opcion == 1:
-        print("\nSu saldo actual es: $", saldo)
-        interacciones = interacciones + 1
-
-    elif opcion == 2:
-        print("\nPromociones disponibles:")
-        print("- 2x1 en recargas")
-        print("- 30% de descuento en datos móviles")
-        interacciones = interacciones + 1
+    match opcion:
         
-    elif opcion == 3:
-        recargas = recargas + 1
-        if recargas < 3:
-            monto = int(input("Ingrese monto a recargar: "))
-            if monto < 0:
-                print("Ingrese un monto positivo")
-            else:
-                saldo = saldo + monto
-            print("Recarga exitosa. Nuevo saldo: $", saldo)
+        case 1:
+            print("\nSu saldo actual es: $", saldo)
             interacciones = interacciones + 1
-        else:
-            print("Cantidad de recargas superadas")
+
+        case 2:
+            print("\nPromociones disponibles:")
+            print("- 2x1 en recargas")
+            print("- 30% de descuento en datos móviles")
+            interacciones = interacciones + 1
+        
+        case 3:
+            recargas = recargas + 1
+            if recargas < 3:
+                monto = int(input("Ingrese monto a recargar: "))
+                if monto < 0:
+                    print("Ingrese un monto positivo")
+                else:
+                    saldo = saldo + monto
+                print("Recarga exitosa. Nuevo saldo: $", saldo)
+                interacciones = interacciones + 1
+            else:
+                print("Cantidad de recargas superadas")
             
         
-    elif opcion == 4:
-        print("\nDatos consumidos: ", DATOS_CONSUMIDOS)
-        interacciones = interacciones + 1
+        case 4:
+            print("\nDatos consumidos: ", DATOS_CONSUMIDOS)
+            interacciones = interacciones + 1
         
-    elif opcion == 5:
-        print("\nGracias por comunicarse. ¡Hasta luego!")
-        print("Tu cantidad de interacciones fue:", interacciones) 
+        case 5:
+            print("\nGracias por comunicarse. ¡Hasta luego!")
+            print("Tu cantidad de interacciones fue:", interacciones) 
     
-    else:
-        print("Opción inválida. Intente nuevamente.")
+        case _:
+            print("Opción inválida. Intente nuevamente.")
