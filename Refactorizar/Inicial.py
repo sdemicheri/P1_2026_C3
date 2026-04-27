2f0fe233c96a066f5d5bee050cff7811e9e87903
Miqueas17 <miqueasbarrozo12@gmail.com>
2026-04-27 17:13:28 -0300

    Agregado funcionalidad para ver el Credito Gastado

--- Refactorizar/Inicial.py
+++ Refactorizar/Inicial.py
@@ -1,3 +1,4 @@
+CREDITO_GASTADO = 0
 opcion = 0
 saldo = 10000
 
@@ -7,25 +8,32 @@ while opcion != 4:
     print("1. Consultar saldo")
     print("2. Ver promociones")
     print("3. Recargar saldo")
+    print("4. Averiguar Credito Gastado")
     print("4. Salir")
+    try:
+        opcion = int(input("Seleccione una opción: "))
 
-    opcion = int(input("Seleccione una opción: "))
+        if opcion == 1:
+            print("\nSu saldo actual es: $", saldo)
 
-    if opcion == 1:
-        print("\nSu saldo actual es: $", saldo)
+        elif opcion == 2:
+            print("\nPromociones disponibles:")
+            print("- 2x1 en recargas")
+            print("- 30% de descuento en datos móviles")
 
-    elif opcion == 2:
-        print("\nPromociones disponibles:")
-        print("- 2x1 en recargas")
-        print("- 30% de descuento en datos móviles")
+        elif opcion == 3:
+            try:
+                monto = int(input("Ingrese monto a recargar: "))
+                saldo = saldo + monto
+                print("Recarga exitosa. Nuevo saldo: $", saldo)
+            except ValueError:
+                print("Ingrese un valor valido")
+        elif opcion == 4:
+            print("Su credito gastado es: ",  CREDITO_GASTADO)
+        elif opcion == 5:
+            print("\nGracias por comunicarse. ¡Hasta luego!")
 
-    elif opcion == 3:
-        monto = int(input("Ingrese monto a recargar: "))
-        saldo = saldo + monto
-        print("Recarga exitosa. Nuevo saldo: $", saldo)
-
-    elif opcion == 4:
-        print("\nGracias por comunicarse. ¡Hasta luego!")
-
-    else:
-        print("Opción inválida. Intente nuevamente.")
+        else:
+            print("Opción inválida. Intente nuevamente.")
+    except ValueError:
+        print("Ingrese un valor valido")
